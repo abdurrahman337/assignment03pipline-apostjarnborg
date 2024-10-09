b190999fd88a4db9a9b2e918a136be075ce0802e
Abdurrahman Stjärnborg <apo.b@hotmail.se>
2024-10-09 22:49:00 +0200

    Create Client

--- tests/testsuit.spec.ts
+++ tests/testsuit.spec.ts
@@ -8,7 +8,25 @@ test.describe('Front-end tests', () => {
       await page.locator('input[type="text"]').fill(`${process.env.TEST_USERNAME}`);
       await page.locator('input[type="password"]').fill(`${process.env.TEST_PASSWORD}`);
       await page.getByRole('button', { name: 'Login' }).click();
-    
+      await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible();
+      await page.locator('div').filter({ hasText: /^ClientsNumber: 2View$/ }).getByRole('link').click();
+      await page.waitForTimeout(4000);
+      await page.getByRole('link', { name: 'Create Client' }).click();
+      await page.waitForTimeout(4000);
+      await page.locator('div').filter({ hasText: /^Name$/ }).getByRole('textbox').fill('Alex');
+      await page.locator('input[type="email"]').fill('alex.b@hotmail.se');
+      await page
+        .locator('div')
+        .filter({ hasText: /^Telephone$/ })
+        .getByRole('textbox')
+        .fill('0755885555');
+      await page.getByText('Save').click();
+      await page.waitForTimeout(4000);
+      const thirdImage = page.getByRole('img').nth(2);
+      await expect(thirdImage).toBeVisible(); // Kontrollera att bilden är synlig
+      // Verifiera att rubriken 'Alex (#3)' är synlig
+      const clientHeading = page.getByRole('heading', { name: 'Alex (#3)' });
+      await expect(clientHeading).toBeVisible();
 
 
 
