c528b27fab53eba5c7fe0b1ee00dc2cd86589f98
Abdurrahman Stjärnborg <apo.b@hotmail.se>
2024-10-10 17:51:42 +0200

    Create Room

--- tests/testsuit.spec.ts
+++ tests/testsuit.spec.ts
@@ -34,37 +34,45 @@ test.describe('Front-end tests', () => {
     });
 
 
-    /*
-        test('Create a Room', async ({ page }) => {
-          await page.goto('http://localhost:3000'); // Ensure the correct port number
-          await page.locator('input[type="text"]').fill(`${process.env.TEST_USERNAME}`);
-          await page.locator('input[type="password"]').fill(`${process.env.TEST_PASSWORD}`);
-          await page.getByRole('button', { name: 'Login' }).click();
-          await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible();
-          await page.locator('div').filter({ hasText: /^RoomsNumber: 2View$/ }).getByRole('link').click();
-          await page.waitForTimeout(4000);
-          // Klicka på länken för att skapa ett nytt rum
-          await page.getByRole('link', { name: 'Create Room' }).click();
-          await page.waitForTimeout(4000); // Vänta för att säkerställa att sidan är laddad
+
+    test('Create a Room', async ({ page }) => {
+      await page.goto('http://localhost:3000'); // Ensure the correct port number
+      await page.locator('input[type="text"]').fill(`${process.env.TEST_USERNAME}`);
+      await page.locator('input[type="password"]').fill(`${process.env.TEST_PASSWORD}`);
+      await page.getByRole('button', { name: 'Login' }).click();
+      await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible();
+      await page.locator('div').filter({ hasText: /^RoomsNumber: 2View$/ }).getByRole('link').click();
+      await page.waitForTimeout(4000);
+      // Klicka på länken för att skapa ett nytt rum
+      await page.getByRole('link', { name: 'Create Room' }).click();
+      await page.waitForTimeout(4000);
+      // Hämta kombinationsfältet för rumsalternativ
+      const select = page.locator('#app > div > div:nth-child(2) > div:nth-child(1) > select');
+      await select.selectOption({ index: 1 }); // Välj det andra alternativet (index 1)
+      const selectedOption = await select.locator('option:checked').innerText();
+      console.log('Valt alternativ:', selectedOption);
+      await expect(selectedOption).toBe(' Single ');
+      await page.locator('div').filter({ hasText: /^Number$/ }).getByRole('spinbutton').fill('3');
+      await page.locator('div').filter({ hasText: /^Floor$/ }).getByRole('spinbutton').fill('3');
+      await page.waitForTimeout(4000);
+      await page.locator('.checkbox').click();
+      // Optionally, verify if the checkbox action was successful
+      const checkboxChecked = await page.locator('.checkbox').evaluate(div => {
+        return div.classList.contains('checked'); // Adjust this check based on your CSS class
+      });
+      await page.locator('div').filter({ hasText: /^Price$/ }).getByRole('spinbutton').fill('3500');
+      const selectFeatures = page.locator('select[multiple="multiple"]');
+      await selectFeatures.selectOption('sea_view');
+      await page.getByText('Save').click();
+      await page.waitForTimeout(4000);
+      const heading = await page.getByRole('heading', { name: 'Floor 3, Room' });
+      await expect(heading).toBeVisible();
+
+
+
+
+    });
     
-          // Hämta kombinationsfältet för rumsalternativ
-          const select = page.locator('#app > div > div:nth-child(2) > div:nth-child(1) > select');
-    
-          // Välj det andra alternativet (t.ex. "triple")
-          await select.selectOption({ index: 1 }); // Välj det andra alternativet (index 1)
-    
-          // Hämta texten för det valda alternativet
-          const selectedOption = await select.locator('option:checked').innerText();
-          console.log('Valt alternativ:', selectedOption);
-          await expect(selectedOption).toBe(' Single ');
-          await page.locator('div').filter({ hasText: /^Number$/ }).getByRole('spinbutton').fill('3');
-          await page.locator('div').filter({ hasText: /^Floor$/ }).getByRole('spinbutton').fill('3');
-          await page.waitForTimeout(4000); // Vänta för att säkerställa att sidan är laddad
-          await page.locator('.checkbox').check();
-    
-    
-    
-        });*/
   });
 
 });
