713ba3990b10124d0af28285ba9aadca5941994c
Abdurrahman Stjärnborg <apo.b@hotmail.se>
2024-10-10 16:35:55 +0200

    senast

--- tests/testsuit.spec.ts
+++ tests/testsuit.spec.ts
@@ -31,6 +31,38 @@ test.describe('Front-end tests', () => {
 
 
 
+    });
+
+
+
+    test('Create a Room', async ({ page }) => {
+      await page.goto('http://localhost:3000'); // Ensure the correct port number
+      await page.locator('input[type="text"]').fill(`${process.env.TEST_USERNAME}`);
+      await page.locator('input[type="password"]').fill(`${process.env.TEST_PASSWORD}`);
+      await page.getByRole('button', { name: 'Login' }).click();
+      await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible();
+      await page.locator('div').filter({ hasText: /^RoomsNumber: 2View$/ }).getByRole('link').click();
+      await page.waitForTimeout(4000);
+      // Klicka på länken för att skapa ett nytt rum
+      await page.getByRole('link', { name: 'Create Room' }).click();
+      await page.waitForTimeout(4000); // Vänta för att säkerställa att sidan är laddad
+
+      // Hämta kombinationsfältet för rumsalternativ
+      const select = page.locator('#app > div > div:nth-child(2) > div:nth-child(1) > select');
+
+      // Välj det andra alternativet (t.ex. "triple")
+      await select.selectOption({ index: 1 }); // Välj det andra alternativet (index 1)
+
+      // Hämta texten för det valda alternativet
+      const selectedOption = await select.locator('option:checked').innerText();
+      console.log('Valt alternativ:', selectedOption);
+      await expect(selectedOption).toBe(' Single ');
+      await page.locator('div').filter({ hasText: /^Number$/ }).getByRole('spinbutton').fill('3');
+      await page.locator('div').filter({ hasText: /^Floor$/ }).getByRole('spinbutton').fill('3');
+      await page.waitForTimeout(4000); // Vänta för att säkerställa att sidan är laddad
+      await page.locator('.checkbox').check();
+
+
 
     });
   });
